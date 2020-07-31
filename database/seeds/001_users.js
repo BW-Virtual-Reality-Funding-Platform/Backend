dd7e969c6862653c28d422152b34004640d7c179
jaren <jarensmart@yahoo.com>
2020-07-31 10:15:56 -0400

    Fixed test users 1-4

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -1,8 +1,11 @@
+const bcrypt = require("bcryptjs");
+const hash = async (password) => bcrypt.hash(password, 3);
+
 exports.seed = async function (knex) {
   await knex("users").truncate();
   await knex("users").insert([
-    { username: "test1", password: "test1" },
-    { username: "test2", password: "test2" },
-    { username: "test3", password: "test3" },
+    { username: "test1", password: `${await hash("test1")}` },
+    { username: "test2", password: `${await hash("test2")}` },
+    { username: "test3", password: `${await hash("test3")}` },
   ]);
 };
